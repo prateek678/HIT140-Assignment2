a38ba15dcdef3c499d879d374b122323843e9eb6
Prateek Bryan Chapagai <71961475+prateek678@users.noreply.github.com>
2025-09-10 20:05:47 +1000

    Update Assessment-2.py
    Cleaned outliers and analyzed two sample t-test between bat landing to food and risk taken or not.

--- Assessment-2.py
+++ Assessment-2.py
@@ -80,4 +80,101 @@ plt.show()
 """
     There are huge number of outliers in bat_landing_to_food let's remove them to clean the data. These outliers may affect our final result.
 """
+#Cleaning outliers
+Q1 = d1['bat_landing_to_food'].quantile(0.25)
+Q3 = d1['bat_landing_to_food'].quantile(0.75)
+IQR =  Q3 - Q1
 
+lower_bound = Q1 - 1.5*IQR
+upper_bound = Q1 + 1.5 * IQR
+
+filtered_d1 = d1[(d1['bat_landing_to_food'] >= lower_bound) & (d1['bat_landing_to_food'] <= upper_bound)]
+print(filtered_d1.describe())
+
+"""
+    Let's see if bats show hesitation to approach food and take risks or not. If they show more hesitation then they perceive bats as predators.
+    Else they perceive rats as only competitors.
+    For this we have to use t-test. TO compare if bat hesitates more or less.
+   
+    Since 'bat_landing_to_food' and 'risk' has higher correlation, let's calculate two sample t-test.
+"""
+
+#Case 1: bat_landing_to_food vs risk taken or not
+sample1 = filtered_d1.loc[d1["risk"]==1, "bat_landing_to_food"]
+sample2 = filtered_d1.loc[d1["risk"]==0, "bat_landing_to_food"]
+
+mean1 = sample1.mean()
+mean2 = sample2.mean()
+std1 = sample1.std()
+std2 = sample2.std()
+n1 = len(sample1)
+n2 = len(sample2)
+
+
+"""
+    Calculating two sample mean t-test, where sample1 has hesitation time of bats and taking risk.
+    sample2 has hesitationi time of bats and not taking risk.
+   
+    Null Hypothesis: The number of risk taking bats are equal with number of bats not taking risk
+    Alternative Hypothesis: The number of risk taking bats are not equal with number of risk not taking bats.
+"""
+t_stat, p_value = st.ttest_ind_from_stats(mean1, std1, n1, mean2, std2, n2, equal_var=False)
+print("T-stats dataset 1",t_stat) #6.682
+print("P-value dataset 1", p_value) #5.72x10^-11
+
+print("Mean 1", mean1) #5.1686 seconds
+print("Mean 2", mean2) #3.1159 seconds
+
+"""
+    Here p-value is too small which indicates that we reject the null hypothesis. Thus there are not
+    equal number of bats that take risk and not taking risk. Analyzing mean we found that risk taking
+    bats hesitates more before approaching food (5.1686 seconds) than risk not taking bats (3.11 secs)
+   
+    This hesitation means that bats hesitates more before approaching food platform when rats are
+    present.
+"""
+
+
+#FOR DATASET 2
+
+print(d2.info())
+
+#Check correlation among numerical features.
+plt.figure(figsize=(8,6))
+numerical_columns = d2.select_dtypes(include=['int64','float64'])
+sns.heatmap(numerical_columns.corr(), annot=True, cmap='coolwarm')
+plt.title("Dataset 2 correlation among numerical features")
+plt.show()
+
+"""
+    Correlation matrix shows that 'bat_landing_number' and 'food_availability' has high correlation.
+    However, 'bat_landing_number' and 'rat_minutes' are negatively correlated.
+"""
+
+print(d2['food_availability'].describe())
+ct = pd.crosstab(d2['food_availability'],d2['bat_landing_number'])
+
+#Distribution of food availability and bat landing number
+plt.figure(figsize=(8,6))
+plt.scatter(d2['food_availability'],d2['bat_landing_number'])
+plt.title("Distribution of 'food_availability' and 'bat_landing_number'")
+plt.xlabel("Food Availability")
+plt.ylabel("Bat_landing_number")
+plt.show()
+
+
+#Distribution of food availability and rat arrival number
+plt.figure(figsize=(8,6))
+plt.scatter(d2['rat_arrival_number'],d2['food_availability'])
+plt.title("Distribution of 'food_availability' and 'rat_arrival_number'")
+plt.xlabel("Rat Arrival Number")
+plt.ylabel("Food Availability")
+plt.show()
+
+#Distribution of rat arrival number and bat landing number
+plt.figure(figsize=(8,6))
+plt.scatter(d2['bat_landing_number'], d2['rat_arrival_number'])
+plt.title("Distribution of 'rat_arrival_number' and 'bat_landing_number'")
+plt.xlabel("Bat Landing Number")
+plt.ylabel("Rat Arrival Number")
+plt.show()
